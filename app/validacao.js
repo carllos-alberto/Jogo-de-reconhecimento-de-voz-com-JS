e2f76c9062c5fa12377a2a7efe1a11aea2271401
Seu nome <carllos.seg@gmail.com>
2022-10-29 13:06:20 -0300

    Validação e reconhecimento de voz

---
+++ app/validacao.js
@@ -0,0 +1,42 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+  const numero = +chute;
+
+  if (chuteForInvalido(numero)) {
+    elementoChute.innerHTML += `<div>Valor invalido</div>`;
+    return;
+  }
+
+  if (numeroForMaiorOuMenorQueValorPermitido(numero)) {
+    elementoChute.innerHTML += `<div>Valor inválido: Informe número secreto precisa estar entre ${menorValor} e ${maiorValor}</div>`;
+    return;
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+    <h2>Parabéns você acertou!!</h2>
+    <h3>O número secreto era ${numeroSecreto}</h3>
+    
+    <button id="jogar-novamente" class=btn-jogar">Jogar Novamente</button>
+`;
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-sharp fa-solid  fa-arrow-donw"></i></div>
+    `;
+  } else {
+    elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-sharp fa-solid  fa-arrow-up"></i></div>
+    `;
+  }
+}
+
+function chuteForInvalido(numero) {
+  return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueValorPermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar-novamente") {
+    window.location.reload();
+  }
+});
